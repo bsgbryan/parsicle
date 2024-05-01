ba8d9107397ef21b2906f4bf2d705a131faa2a93
Bryan Maynard <bsgbryan@gmail.com>
2024-05-01 00:15:51 -0500

    Added support for lists
    Also beefed up Images and added logic to strip out unwanted paragraph text

--- src/parse.rs
+++ src/parse.rs
@@ -12,8 +12,9 @@ extern crate comrak;
 #[derive(Debug)]
 pub struct ParsedArticle {
     pub content: Vec<Section>,
-    pub images: Vec<String>,
+    pub images: Vec<Image>,
     pub links: Vec<String>,
+    pub lists: Vec<List>,
     pub title: String,
 }
 
@@ -26,6 +27,36 @@ impl Paragraph {
     fn new(text: &str) -> Self {
         Paragraph {
             tokens: vec![text.to_string()],
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Image {
+    caption: Option<String>,
+    title: Option<String>,
+    url: String,
+}
+
+impl Image {
+    fn new(url: String) -> Self {
+        Image {
+            caption: None,
+            title: None,
+            url,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct List {
+    items: Vec<String>,
+}
+
+impl List {
+    fn new() -> Self {
+        List {
+            items: vec![],
         }
     }
 }
@@ -51,22 +82,28 @@ impl Section {
 
 #[derive(Debug)]
 struct Content {
-    new_heading: bool,
     ignore: bool,
+    new_heading: bool,
+    new_image: bool,
+    new_list_item: bool,
     new_paragraph: bool,
-    images: Vec<String>,
+    images: Vec<RefCell<Image>>,
     links: Vec<String>,
+    lists: Vec<RefCell<List>>,
     sections: Vec<RefCell<Section>>,
 }
 
 impl Content {
     fn new() -> Self {
         Content {
-            new_heading: false,
             ignore: false,
             images: vec![],
+            new_heading: false,
+            new_image: false,
             new_paragraph: false,
+            new_list_item: false,
             links: vec![],
+            lists: vec![],
             sections: vec![RefCell::new(Section {
                 content: vec![],
                 heading: String::new(),
@@ -90,25 +127,27 @@ where
 }
 
 fn append_text(content: &mut Content, text: &str) {
-    let mut section = content.
-        sections.
-        last().
-        unwrap().
-        borrow_mut();
-
-    let paras: &mut Vec<RefCell<Paragraph>> = section.
-        paragraphs.
-        as_mut();
-
-    if text.to_lowercase().starts_with("read more") {
-        content.ignore = true;
-    } else {
-        paras.
+    if text.split(" ").count() > 1 {
+        let mut section = content.
+            sections.
             last().
             unwrap().
-            borrow_mut().
-            tokens.
-            push(text.to_string());
+            borrow_mut();
+    
+        let paras: &mut Vec<RefCell<Paragraph>> = section.
+            paragraphs.
+            as_mut();
+    
+        if text.to_lowercase().starts_with("read more") {
+            content.ignore = true;
+        } else {
+            paras.
+                last().
+                unwrap().
+                borrow_mut().
+                tokens.
+                push(text.to_string());
+        }
     }
 }
 
@@ -116,6 +155,11 @@ fn finish(section: &mut RefMut<Section>) {
     section.content = section.
         paragraphs.
         iter().
+        filter(|p| {
+            let tokens = &p.borrow().tokens;
+
+            tokens.len() > 1 || (tokens.len() > 0 && tokens.last().unwrap().ends_with("."))
+        }).
         map(|p| p.
             borrow().
             tokens.
@@ -141,14 +185,16 @@ fn create_new_section(content: &mut Content, text: &str) {
 }
 
 fn create_new_paragraph(content: &mut Content, text: &str) {
-    content.
-        sections.
-        last().
-        unwrap().
-        borrow_mut().
-        paragraphs.
-        push(RefCell::new(Paragraph::new(text))
-    );
+    if text.split(" ").count() > 1 {
+        content.
+            sections.
+            last().
+            unwrap().
+            borrow_mut().
+            paragraphs.
+            push(RefCell::new(Paragraph::new(text))
+        );
+    }
 
     content.new_paragraph = false;
 }
@@ -167,7 +213,11 @@ async fn fetch_and_parse(url: &str) -> Result<(String, String), Box<dyn Error>> 
 }
 
 pub async fn parse<'a>(url: &str) -> Result<ParsedArticle, Box<dyn std::error::Error>> {
-    let (title, md) = fetch_and_parse(url).await.ok().unwrap();
+    let (title, md) = fetch_and_parse(url).
+        await.
+        ok().
+        unwrap();
+
     let arena = Arena::new();
     let root = parse_document(&arena, &md, &Options::default());
 
@@ -181,25 +231,71 @@ pub async fn parse<'a>(url: &str) -> Result<ParsedArticle, Box<dyn std::error::E
             node,
         | match &node.data.borrow().value {
             NodeValue::Image(img) => {
-                content.images.push(img.url.clone());
+                // println!("IMAGE: {} -> {}", img.title, img.url);
+                content.images.push(RefCell::new(Image::new(img.url.clone())));
+                content.new_image = true;
+                content.new_paragraph = false;
             }
             NodeValue::Heading(_) => {
+                // println!("HEADING");
                 content.new_heading = true;
             }
             NodeValue::Text(ref text) => {
+                // println!("TEXT: {}", text);
                 if content.new_heading {
                     create_new_section(content, text);
                 } else {
                     if content.new_paragraph {
-                        create_new_paragraph(content, &text);
+                        if content.new_image {
+                            let mut image = content.
+                                images.
+                                last().
+                                unwrap().
+                                borrow_mut();
+
+                            image.caption = Some(text.to_string());
+
+                            content.new_image = false;
+                        } else if content.new_list_item {
+                            let mut list = content.
+                                lists.
+                                last().
+                                unwrap().
+                                borrow_mut();
+
+                            list.items.push(text.to_string());
+
+                            content.new_list_item = false;
+                        } else {
+                            create_new_paragraph(content, &text);
+                        }
                     } else {
-                        append_text(content, text);
+                        if content.new_image {
+                            let mut image = content.
+                                images.
+                                last().
+                                unwrap().
+                                borrow_mut();
+
+                            image.title = Some(text.to_string());
+                        } else {
+                            append_text(content, text);
+                        }
                     }
                 }
             }
-            // NodeValue::List(_) => {
-            //     println!("Outside a paragraph, got a list!");
-            // }
+            NodeValue::List(_) => {
+                content.new_image = false;
+
+                content.lists.push(RefCell::new(List::new()));
+
+                // println!("LIST");
+            }
+            NodeValue::Item(item) => {
+                // println!("ITEM: {:#?}", item.list_type);
+
+                content.new_list_item = true;
+            }
             NodeValue::Link(ref link) => {
                 if content.ignore == false {
                     content.links.push(link.url.clone());
@@ -208,6 +304,7 @@ pub async fn parse<'a>(url: &str) -> Result<ParsedArticle, Box<dyn std::error::E
                 }
             }
             NodeValue::Paragraph => {
+                // println!("PARAGRAPH");
                 content.new_paragraph = true;
             }
             _ => (),
@@ -225,13 +322,28 @@ pub async fn parse<'a>(url: &str) -> Result<ParsedArticle, Box<dyn std::error::E
         content: content.
             sections.
             iter().
-            map(|s: &RefCell<Section>| s.
+            map(|s| s.
                 borrow().
                 clone()
             ).
             collect(),
-        images: content.images,
+        images: content.
+            images.
+            iter().
+            map(|i| i.
+                borrow().
+                clone()
+            ).
+            collect(),
         links: content.links,
+        lists: content.
+            lists.
+            iter().
+            map(|l| l.
+                borrow().
+                clone()
+            ).
+            collect(),
         title,
     })
 }
